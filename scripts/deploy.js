efc5145676a8a5b90d18cd0368a52556e4da6c27
itsmeRiF <arif.ali.khan12@gmail.com>
2025-07-01 13:38:21 +0530

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,1 +1,13 @@
+async function main() {
+  const LaxmiChitFund = await ethers.getContractFactory("LaxmiChitFund");
+  const fund = await LaxmiChitFund.deploy();
+  await fund.waitForDeployment();
+
+  console.log("LaxmiChitFund deployed to:", fund.target);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
 
